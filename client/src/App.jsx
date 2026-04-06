6b08448d714b9fdedd9b2ed76da86e14edc01e5a
yue7happy <yue7happy@163.com>
2026-04-06 16:20:33 +0800

    Update API_URL to https://api.teamup.us.ci

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback, useRef } from 'react'
 import io from 'socket.io-client'
 import './App.css'
 
-const API_URL = 'https://backend.yue7happy.workers.dev';
+const API_URL = 'https://api.teamup.us.ci';
 const statusColors = {
   matching: '#ea4335',
   gaming: '#34a853',
